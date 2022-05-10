e8711c46b488995e5bd74703ed22cf0dbe8de8c8
unknown <gel.giacomini@gmail.com>
2022-05-10 11:27:48 -0300

    Vai logo

---
+++ Desktop/Trabalho1005/ansuz/src/components/caixaCentro.js
@@ -0,0 +1,18 @@
+import React from "react";
+import style from "./css/style.css";
+
+const Caixa = () => {
+        return (
+            <>
+
+            <div className="caixa">
+
+                <p>asidnaubnoians</p>
+
+            </div>
+
+            </>
+        )
+}
+
+export default Caixa
